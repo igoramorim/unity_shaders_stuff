779cc84e84436ecdaa3304465a6841618457f215
igoramorim <igor340@gmail.com>
2019-04-22 23:18:17 -0300

    Raymarching

---
+++ Assets/Raymarching/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    public Vector3 rotationAngle;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += Vector3.forward * movementSpeed;
+        transform.Rotate(rotationAngle.x, rotationAngle.y, rotationAngle.z, Space.Self);
+    }
+}
---
+++ Assets/Raymarching/RaymarchingCamera.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class RaymarchingCamera : SceneViewFilter {
+
+    [SerializeField]
+    private Shader _shader;
+
+    public Material _raymarchMaterial {
+        get {
+            if (!_raymarchMat && _shader) {
+                _raymarchMat = new Material(_shader);
+                _raymarchMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _raymarchMat;
+        }
+    }
+
+    private Material _raymarchMat;
+
+    public Camera _camera {
+        get {
+            if (!_cam) {
+                _cam = GetComponent<Camera>();
+            }
+            return _cam;
+        }
+    }
+
+    private Camera _cam;
+
+    public Transform _directionalLight;
+
+    public float _maxDistance;
+    public Color _mainColor;
+    public Vector4 _sphere1, _sphere2;
+    public Vector4 _box1;
+    public Vector4 _torus1;
+    public Vector3 _modInterval;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (!_raymarchMaterial) {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        _raymarchMaterial.SetMatrix("_CamFrustum", CamFrustum(_camera));
+        _raymarchMaterial.SetMatrix("_CamToWorld", _camera.cameraToWorldMatrix);
+        _raymarchMaterial.SetFloat("_maxDistance", _maxDistance);
+        _raymarchMaterial.SetVector("_sphere1", _sphere1);
+        _raymarchMaterial.SetVector("_sphere2", _sphere2);
+        _raymarchMaterial.SetVector("_box1", _box1);
+        _raymarchMaterial.SetVector("_torus1", _torus1);
+        _raymarchMat.SetVector("_LightDir", _directionalLight ? _directionalLight.forward : Vector3.down);
+        _raymarchMaterial.SetColor("_mainColor", _mainColor);
+        _raymarchMaterial.SetVector("_modInterval", _modInterval);
+
+        RenderTexture.active = destination;
+        _raymarchMaterial.SetTexture("_MainTex", source);
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        _raymarchMaterial.SetPass(0);
+        GL.Begin(GL.QUADS);
+
+        // BL
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f);
+        // BR
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f);
+        // TR
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);
+        // TL
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    private Matrix4x4 CamFrustum(Camera cam) {
+        Matrix4x4 frustum = Matrix4x4.identity;
+        float fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
+
+        Vector3 goUp = Vector3.up * fov;
+        Vector3 goRight = Vector3.right * fov * cam.aspect;
+
+        Vector3 TL = (-Vector3.forward - goRight + goUp); // top left
+        Vector3 TR = (-Vector3.forward + goRight + goUp); // top right
+        Vector3 BR = (-Vector3.forward + goRight - goUp); // bottom right
+        Vector3 BL = (-Vector3.forward - goRight - goUp); // bottom left
+
+        frustum.SetRow(0, TL);
+        frustum.SetRow(1, TR);
+        frustum.SetRow(2, BR);
+        frustum.SetRow(3, BL);
+
+        return frustum;
+    }
+}
